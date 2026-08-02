f4e67dea09729fb6245f1b62f95449ad57ee5886
Archana Pradhan <archanapradhan0786@gmail.com>
2026-08-02 20:33:43 +0530

    First commit : url changes

--- WebCalc/src/main/java/com/example/demo/Calculator.java
+++ WebCalc/src/main/java/com/example/demo/Calculator.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Calculator {
 	
-@GetMapping("calc/{operation}/{a}/{b}")
+@GetMapping("webCalc/{operation}/{a}/{b}")
 int calculation(@PathVariable String operation,@PathVariable int a,@PathVariable int b)
 {
 	if(operation.equals("add"))
